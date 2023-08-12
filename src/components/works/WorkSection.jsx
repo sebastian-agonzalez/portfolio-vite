5175171f9f6eb635ec67086914da3d09d798af37
Sebastian Gonzalez <onemogen8@gmail.com>
2023-08-12 04:17:50 -0300

    add work section info; code cleanup also

--- src/components/works/WorkSection.jsx
+++ src/components/works/WorkSection.jsx
@@ -2,6 +2,8 @@ import { motion } from "framer-motion";
 import { useLocation } from "wouter";
 import { useEffect, useState } from "react";
 
+import "./WorkSection.css";
+
 import ActionBar from "../action-bar/ActionBar";
 import FadingRoute from "../animated-route/FadingRoute";
 import DividerIcon from "../divider-with-icon/DividerIcon";
@@ -29,6 +31,30 @@ const WorkSection = () => {
     return () => setlocationpath(location);
   }, [location]);
 
+  const animatedScrollingDisclaimer = (
+    <>
+      <div className="my-10"></div>
+      <div id="scrolling-text" className="w-full mx-2 overflow-hidden">
+        {" "}
+        <motion.div
+          initial={{ x: "100%" }}
+          animate={{ x: "-45%" }}
+          transition={{
+            repeat: Infinity,
+            duration: 10,
+            ease: "linear",
+          }}
+          style={{ color: "#ffff003e" }}
+          className="text-md"
+        >
+          <BiHappyBeaming /> <PiWarningBold />{" "}
+          <span className="text-base">Site under construction</span>{" "}
+          <PiWarningBold /> <BiHappyBeaming />
+        </motion.div>
+      </div>
+    </>
+  );
+
   return (
     <FadingRoute ckey={"works"}>
       <article className="mb-8 w-full flex justify-center pr-10 pl-6 mx-auto max-w-3xl">
@@ -39,36 +65,15 @@ const WorkSection = () => {
           <div className="py-5"></div>
           <div className="w-full">
             <h1 className="text-4xl text-center font-extrabold">Projects</h1>
-            <div className="my-10"></div>
 
-            <div id="scrolling-text" className="w-full mx-2 overflow-hidden">
-              {" "}
-              <motion.div
-                initial={{ x: "100%" }}
-                animate={{ x: "-100%" }}
-                transition={{
-                  repeat: Infinity,
-                  duration: 8,
-                  ease: "linear",
-                }}
-                style={{ color: "#ffff003e" }}
-                className="text-md"
-              >
-                <BiHappyBeaming className="inline" />
-                <PiWarningBold className="inline" />
-                <span className="text-base">Site under construction</span>
-                <PiWarningBold className="inline" />
-                <BiHappyBeaming className="inline" />
-              </motion.div>
-            </div>
-
+            {animatedScrollingDisclaimer}
             <div id="content" className="lg:px-5 font-semibold">
               <div className="my-14"></div>
               <ul>
                 <li id="stafftracker">
                   <div className="flex justify-start">
                     <div className="inline-block">
-                      <span className="inline">
+                      <span>
                         <CgAsterisk
                           size={30}
                           className="inline animate-spin-slow mb-1"
@@ -98,19 +103,14 @@ const WorkSection = () => {
                           An app that mimicks a system to keep track of the
                           staff in a company.
                           <br />
-                          Built on{" "}
-                          <span className="text-slate-500">Next.js</span>, uses{" "}
-                          <span className="text-slate-500">zustand</span> for
-                          state management,{" "}
-                          <span className="text-slate-500">framer-motion</span>{" "}
-                          for route animations, css and{" "}
-                          <span className="text-slate-500">tailwind</span>,
-                          flowbite and MUI for styling.
+                          Built on <span>Next.js</span>, uses{" "}
+                          <span>Zustand</span> for state management,{" "}
+                          <span>Framer-Motion</span> for route animations, css
+                          and <span>Tailwind CSS</span>, flowbite and MUI for
+                          styling.
                           <br />
-                          It consumes its own{" "}
-                          <span className="text-slate-500">graphql</span> api
-                          using{" "}
-                          <span className="text-slate-500">apollo-client</span>.
+                          It consumes its own <span>Graphql</span> api using{" "}
+                          <span>Apollo-Client</span>.
                           <br />
                           You can see your staff, go the a view page for each,
                           modify the profiles and add a new register.
@@ -124,7 +124,7 @@ const WorkSection = () => {
                   <div className="my-10"></div>
                   <div className="flex justify-start">
                     <div className="inline-block">
-                      <span className="inline">
+                      <span>
                         <CgAsterisk
                           size={30}
                           className="inline animate-spin-slow mb-1"
@@ -153,14 +153,9 @@ const WorkSection = () => {
                       <div id="stafftracker-description" className="pr-4">
                         <p className="text-justify [word-spacing:4px] tracking-tighter">
                           The API for StaffTracker web app to consume. <br />
-                          Implements{" "}
-                          <span className="text-slate-500">
-                            Apollo Server Library
-                          </span>
-                          , using{" "}
-                          <span className="text-slate-500">GraphQL</span>{" "}
-                          schemas definition with its proper resolvers for
-                          handling CRUD operations.{" "}
+                          Implements <span>Apollo Server Library</span>, using{" "}
+                          <span>GraphQL</span> schemas definition with its
+                          proper resolvers for handling CRUD operations.{" "}
                         </p>
                       </div>
                     </div>
@@ -171,7 +166,7 @@ const WorkSection = () => {
                   <div className="my-10"></div>
                   <div className="flex justify-start">
                     <div className="inline-block">
-                      <span className="inline">
+                      <span>
                         <HiArrowRight
                           size={20}
                           className="inline animate-bounce mx-1"
@@ -186,7 +181,7 @@ const WorkSection = () => {
                         <a
                           className="flex"
                           id="window open"
-                          href="https://stafftracker.vercel.app"
+                          href="https://github.com/sebastian-agonzalez/solid_test/tree/main"
                           rel="noopener noreferrer"
                           target="_blank"
                         >
@@ -210,8 +205,50 @@ const WorkSection = () => {
                     </div>
                   </div>
                 </li>
+                <li id="portfolio">
+                  <h2 className="text-2xl"></h2>
+                  <div className="my-10"></div>
+                  <div className="flex justify-start">
+                    <div className="inline-block">
+                      <span>
+                        <HiArrowRight
+                          size={20}
+                          className="inline animate-bounce mx-1"
+                        ></HiArrowRight>
+                      </span>
+                    </div>
+                    <div className="inline-block">
+                      <div className="flex justify-between">
+                        <h2 className="font-semibold text-xl">Portfolio</h2>
+                        <a
+                          className="flex"
+                          id="window open"
+                          href="/"
+                          rel="noopener noreferrer"
+                          target="_blank"
+                        >
+                          <TbExternalLink size={25} className="inline mx-2" />
+                        </a>
+                      </div>
+                      <div className="my-4"></div>
+                      <div className="pr-4">
+                        <p className="text-justify [word-spacing:2px] tracking-tight">
+                          This very site! Intended as a channel to get in touch
+                          with me and link to some works for reference. Built
+                          with <span>Vite.js</span> with <span>React.js</span>,
+                          uses lightweight router <span>Wouter</span> with{" "}
+                          <span>Tailwind CSS</span> for styling and{" "}
+                          <span>Framer-Motion</span> for animating route changes
+                          and the main logo loop.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </li>
               </ul>
             </div>
+            <div className="py-4"></div>
+            {animatedScrollingDisclaimer}
           </div>
         </div>
       </article>
